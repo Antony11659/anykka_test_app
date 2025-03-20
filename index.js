2bfbbeed48e0113fbd9e297256704482b4c75d21
Anton Shurupov <ant_shur@Antons-MacBook-Air.local>
2025-03-20 19:25:56 +0300

    set up a very simple tg bot'

---
+++ index.js
@@ -0,0 +1,12 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = "7608024573:AAHMupDrsYE2TSY0CE1-N0AgAH3goZ_uCSs";
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  
+  bot.sendMessage(chatId, "Hi how are you ")
+});
